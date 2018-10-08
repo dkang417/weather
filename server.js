214e58cf3db5f7472dc545cdb895eea5492e8351
dkang417 <dkang417@gmail.com>
2018-10-07 21:06:03 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,35 +1,18 @@
 
-const bodyParser = require('body-parser');
+
 const express = require('express');
 const path = require('path');
-
-const port = process.env.PORT || 4200;
 const app = express();
 
 
-const sessionConfig = {
-  saveUninitialized: true,
-  secret: 'secretsecret',
-  resave: false,
-  name: 'session',
-  rolling: true,
-  cookie: {
-    secure: false,
-    httpOnly: false,
-    maxAge: 360000,
-  },
-};
-
-app
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(bodyParser.json())
-  .use(express.static(path.join(__dirname, 'dist/weatherapp')))
-  .use(cookieParser(';alkdfjladjfldsajfhsad'))
-  .use(session(sessionConfig))
-  .use('/api', require('./server/routes'))
-  .use(require('./server/routes/catch-all.routes'));
 
 
-app.listen(port, () => console.log(`Express server listening on port ${port}`));
 
 
+app.use(express.static(__dirname + '/dist/weatherapp'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/weatherapp/index.html'));
+});
+
+app.listen(process.env.PORT || 4200);
